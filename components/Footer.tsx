0588936936c1c29527a347103a01a026b98af61c
INDRAREDDY\indra <indrakaveli@gmail.com>
2025-09-26 21:02:48 +0530

    Changed Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,8 @@ export default function Footer() {
   return (
     <footer>
       <p>&copy; 2025 Real-Time Orderbook</p>
+      <p>All rights reserved.</p>
+      <p>Contact us at</p>
     </footer>
   );
 }
